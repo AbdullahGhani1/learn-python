c275f1f831326c6546affc58a8433edef5ea87e4
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2025-08-18 15:52:14 +0500

    model and admin

--- django/storefront/tempes/models.py
+++ django/storefront/tempes/models.py
@@ -1,3 +1,31 @@
+from __future__ import annotations
+from datetime import datetime
 from django.db import models
-
+from typing import TYPE_CHECKING
+from django.utils import timezone
 # Create your models here.
+
+
+if TYPE_CHECKING:
+    from django.db.models import CharField, DateTimeField, ImageField
+
+
+class Posts(models.Model):
+    PROGRAMMING_LANG_CHOICE = [
+        ('PY', 'PYTHON'),
+        ('JS', 'JAVA SCRIPT'),
+        ('C#', 'C SHARP'),
+        ('TS', 'TYPE SCRIPT'),
+    ]
+    title: CharField[str, str]
+    image: ImageField
+    date_added: DateTimeField[datetime, datetime]
+    type: CharField[str, str]
+
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='temps/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=PROGRAMMING_LANG_CHOICE)
+
+    def __str__(self) -> str:
+        return self.title
